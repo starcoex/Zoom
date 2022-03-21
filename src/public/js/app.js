c7862201fbae657d22a9d82114605e2952ac7121
starcoex <69073390+starcoex@users.noreply.github.com>
2022-03-21 13:33:54 +0900

    first Server Client Connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,2 @@
-console.log("Client Start");
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`); //Server에서 가져온 정보를 socket 변수에 저장
+console.log(socket);
--- src/server.js
+++ src/server.js
@@ -21,6 +21,11 @@ app.get("*", handleRedirect);
 
 const server = http.createServer(app); //http 서버
 const wss = new WebSocket.Server({ server }); // http 서버에 WebSocket으로 랜더링 하기 위해서
+function handleConnection(socket) {
+  // FronEnd에서 정보가 socket이라는 변수로 전달됨, 서버에 socket은 연결된 브라우저 즉 frontend
+  console.log(socket);
+}
+wss.on("connection", handleConnection);
 server.listen(PORT, () => {
   console.log(`Server Start http://localhost:${PORT}`);
 });
