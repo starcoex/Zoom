160e186c1cf38bbaef37f0a55164f8438996f92b
starcoex <69073390+starcoex@users.noreply.github.com>
2022-03-22 00:22:07 +0900

    first socket.io start

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,45 +1,81 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
-const socket = new WebSocket(`ws://${window.location.host}`); //Server에서 가져온 정보를 socket 변수에 저장
+// Socket.io Client Start
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-}
+const socketio = io();
 
-// Server에서 보내는 메세지 받기
-socket.addEventListener("open", () => {
-  console.log("Server Connected");
-});
-socket.addEventListener("message", (servermessage) => {
-  console.log(servermessage.data);
+const welcome = document.querySelector("#welcome");
+const room = document.querySelector("#room");
+const form = welcome.querySelector("form");
+
+let roomName;
+room.hidden = true;
+function addMessage(message) {
+  const ul = room.querySelector("ul");
   const li = document.createElement("li");
-  li.innerText = servermessage.data;
-  messageList.appendChild(li);
-});
-socket.addEventListener("close", () => {
-  console.log("Server Die");
-});
-function handleSubmit(event) {
-  event.preventDefault();
-  const input = messageForm.querySelector("input");
-  // socket.send(input.value);
-  socket.send(makeMessage("new_message", input.value));
-  input.value = "";
-  // messageList.setAttribute = "li";
-  // messageList.appendChild = input.value;
-  // console.log(messageList);
+  console.log(li.value);
+  li.innerHTML = message;
+  ul.append(li);
 }
-function handleNickSubmit(event) {
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerHTML = `Room : ${roomName} `;
+}
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = nickForm.querySelector("input");
-  // socket.send({
-  //   type: "nickname",
-  //   payload: input.value,
-  // }); //nick, message 구분하기 위해 JSON 형식으로 보냄
-  socket.send(makeMessage("nickname", input.value));
+  const input = form.querySelector("input");
+  socketio.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+
+form.addEventListener("submit", handleRoomSubmit);
+socketio.on("welcome", () => {
+  addMessage("Welcome to Jeju");
+});
+
+// const messageList = document.querySelector("ul");
+// const messageForm = document.querySelector("#message");
+// const nickForm = document.querySelector("#nick");
+// const socket = new WebSocket(`ws://${window.location.host}`); //Server에서 가져온 정보를 socket 변수에 저장
+
+// function makeMessage(type, payload) {
+//   const msg = { type, payload };
+//   return JSON.stringify(msg);
+// }
+
+// // Server에서 보내는 메세지 받기
+// socket.addEventListener("open", () => {
+//   console.log("Server Connected");
+// });
+// socket.addEventListener("message", (servermessage) => {
+//   console.log(servermessage.data);
+//   const li = document.createElement("li");
+//   li.innerText = servermessage.data;
+//   messageList.appendChild(li);
+// });
+// socket.addEventListener("close", () => {
+//   console.log("Server Die");
+// });
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const input = messageForm.querySelector("input");
+//   // socket.send(input.value);
+//   socket.send(makeMessage("new_message", input.value));
+//   input.value = "";
+//   // messageList.setAttribute = "li";
+//   // messageList.appendChild = input.value;
+//   // console.log(messageList);
+// }
+// function handleNickSubmit(event) {
+//   event.preventDefault();
+//   const input = nickForm.querySelector("input");
+//   // socket.send({
+//   //   type: "nickname",
+//   //   payload: input.value,
+//   // }); //nick, message 구분하기 위해 JSON 형식으로 보냄
+//   socket.send(makeMessage("nickname", input.value));
+//   input.value = "";
+// }
+// messageForm.addEventListener("submit", handleSubmit);
+// nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import express from "express";
-import WebSocket from "ws";
+// import WebSocket from "ws";
 import http from "http";
-import { copyFileSync } from "fs";
-
+import SocketIO from "socket.io";
 const app = express();
 
 const PORT = 4000;
@@ -20,31 +19,50 @@ const handleRedirect = (req, res) => {
 app.get("/", home);
 app.get("*", handleRedirect);
 
-const server = http.createServer(app); //http 서버
-const wss = new WebSocket.Server({ server }); // http 서버에 WebSocket으로 랜더링 하기 위해서
-const socketsDb = [];
-wss.on("connection", (socket) => {
-  socketsDb.push(socket);
-  socket["nickname"] = "Anon";
-  // FronEnd에서 정보가 socket이라는 변수로 전달됨, 서버에 socket은 연결된 브라우저 즉 frontend
-  socket.on("close", () => {
-    console.log("Client Die");
+const httpServer = http.createServer(app); //http 서버
+const socketIoServer = SocketIO(httpServer);
+
+socketIoServer.on("connection", (socketio) => {
+  // console.log(socketio);
+  socketio.onAny((event) => {
+    console.log(event);
   });
-  socket.on("message", (clientMessage) => {
-    const parsed = JSON.parse(clientMessage);
-    // console.log(parsed, clientMessage.toString());
-    if (parsed.type === "new_message") {
-      socketsDb.forEach((socketdb) =>
-        socketdb.send(`${socket.nickname}: ${parsed.payload}`)
-      );
-    } else if (parsed.type === "nickname") {
-      // socketsDb.forEach((socketdb) => socketdb.send(parsed.payload));
-      socket["nickname"] = parsed.payload;
-      // console.log(socket);
-    }
+  socketio.on("enter_room", (roomName, done) => {
+    console.log(socketio.rooms);
+    socketio.join(roomName);
+    console.log(socketio.rooms);
+    done();
+    socketio.to(roomName).emit("welcome");
   });
-  // socket.send("Server Send"); //Server에서 메세지 보내기
 });
-server.listen(PORT, () => {
+
+// socket.io start
+
+// const wss = new WebSocket.Server({ server }); // http 서버에 WebSocket으로 랜더링 하기 위해서
+// const socketsDb = [];
+// wss.on("connection", (socket) => {
+//   socketsDb.push(socket);
+//   socket["nickname"] = "Anon";
+//   // FronEnd에서 정보가 socket이라는 변수로 전달됨, 서버에 socket은 연결된 브라우저 즉 frontend
+//   socket.on("close", () => {
+//     console.log("Client Die");
+//   });
+//   socket.on("message", (clientMessage) => {
+//     const parsed = JSON.parse(clientMessage);
+//     // console.log(parsed, clientMessage.toString());
+//     if (parsed.type === "new_message") {
+//       socketsDb.forEach((socketdb) =>
+//         socketdb.send(`${socket.nickname}: ${parsed.payload}`)
+//       );
+//     } else if (parsed.type === "nickname") {
+//       // socketsDb.forEach((socketdb) => socketdb.send(parsed.payload));
+//       socket["nickname"] = parsed.payload;
+//       // console.log(socket);
+//     }
+//   });
+//   // socket.send("Server Send"); //Server에서 메세지 보내기
+// });
+
+httpServer.listen(PORT, () => {
   console.log(`Server Start http://localhost:${PORT}`);
 });
