529398de32e7d6423328e3ba79fb5995044003c5
starcoex <69073390+starcoex@users.noreply.github.com>
2022-03-21 12:53:18 +0900

    first Websocket Server http server setting

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
-import res from "express/lib/response";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
 const PORT = 4000;
-
+// const ws = new WebSocket();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
@@ -12,8 +13,14 @@ app.use("/public", express.static(__dirname + "/public"));
 const home = (req, res) => {
   res.render("home");
 };
+const handleRedirect = (req, res) => {
+  res.redirect("/");
+};
 app.get("/", home);
+app.get("*", handleRedirect);
 
-app.listen(PORT, function () {
+const server = http.createServer(app); //http 서버
+const wss = new WebSocket.Server({ server }); // http 서버에 WebSocket으로 랜더링 하기 위해서
+server.listen(PORT, () => {
   console.log(`Server Start http://localhost:${PORT}`);
 });
